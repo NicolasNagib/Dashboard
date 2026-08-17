7c85502d0eca1d89603e2a5a04ae85843debeb9e
Nagib <nicolas.nagib.dev@gmail.com>
2026-08-16 21:57:23 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,26 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
 
 def conectar():
+
+    # Quando estiver no Streamlit Cloud
+    if "DB_HOST" in st.secrets:
+
+        return psycopg2.connect(
+            host=st.secrets["DB_HOST"],
+            port=st.secrets["DB_PORT"],
+            database=st.secrets["DB_NAME"],
+            user=st.secrets["DB_USER"],
+            password=st.secrets["DB_PASSWORD"],
+            connect_timeout=10
+        )
+
+    # Quando estiver rodando localmente
     return psycopg2.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
